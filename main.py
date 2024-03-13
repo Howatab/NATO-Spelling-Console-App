c1b5064587607928dc9587a5a702cc09c9ea003f
Muhammad Howatab <Howdyhawt@gmail.com>
2024-03-13 19:01:39 +0500

    Try Catch Blocks

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import pandas
 Nato_alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
 Nato_Dict = {row.letter : row.code for (index,row) in Nato_alphabets.iterrows()}
 
-User_Input = input("Enter the word : ")
-User_word = [ letter.upper() for letter in User_Input if letter.isalpha()]
-print(User_word)
+def generate_nato():
+    User_Input = input("Enter the word : ").upper()
+    try:
+        Nato_Code = [Nato_Dict[letter] for letter in User_Input]
+    except KeyError:
+        print("Enter alphabets only")
+        generate_nato()
+    else:
+        
+        print(Nato_Code)
 
-
-
-Nato_Code = [Nato_Dict[letter] for letter in User_word]
-print(Nato_Code)
+generate_nato()
